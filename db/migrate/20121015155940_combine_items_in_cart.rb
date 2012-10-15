35a68d51ef77f81cfa928ce971f98613eb7b4a6d
myappleguy <myappleguy@gmail.com>
2012-10-15 11:30:01 -0500

    added combine items migration

--- db/migrate/20121015155940_combine_items_in_cart.rb
+++ db/migrate/20121015155940_combine_items_in_cart.rb
@@ -1,7 +1,24 @@
 class CombineItemsInCart < ActiveRecord::Migration
+
   def up
-  end
+    # replace multiple items for a single item in cart w/ quantity
+    Cart.all.each do |cart|
+      # count the number of each product in cart
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+      sums.each do |product_id, quantity|
+        if quantity > 1
+          # remove additional items
+          cart.line_items.where(product_id: product_id).delete_all
+
+          # replace with single item
+          cart.line_items.create(product_id: product_id, quantity: quantity)
+        end
+      end
+    end
+  end #end up
 
   def down
-  end
+    
+  end #end down
+  
 end
