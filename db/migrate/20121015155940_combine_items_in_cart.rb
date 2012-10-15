35ebca08c400caafadf5aa966fe4a444bed825b9
myappleguy <myappleguy@gmail.com>
2012-10-15 11:52:01 -0500

    fixed db:migrate => can't mass assign again

--- db/migrate/20121015155940_combine_items_in_cart.rb
+++ db/migrate/20121015155940_combine_items_in_cart.rb
@@ -5,20 +5,24 @@ class CombineItemsInCart < ActiveRecord::Migration
     Cart.all.each do |cart|
       # count the number of each product in cart
       sums = cart.line_items.group(:product_id).sum(:quantity)
+
       sums.each do |product_id, quantity|
         if quantity > 1
           # remove additional items
           cart.line_items.where(product_id: product_id).delete_all
 
           # replace with single item
-          cart.line_items.create(product_id: product_id, quantity: quantity)
+          item = cart.line_items.build(product_id: product_id)
+          item.quantity = quantity
+          item.save!
         end
-      end
-    end
+      end #end sums.each
+
+    end #end Cart.all.each
   end #end up
 
   def down
-    
+
   end #end down
-  
+
 end
