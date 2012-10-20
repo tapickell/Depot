e84d1554279a415de665e517a813e158af019f01
myappleguy <myappleguy@gmail.com>
2012-10-19 21:02:25 -0500

    getting cart to work, looks god but got some tests to get working

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ActiveRecord::Base
   attr_accessible :address, :email, :name, :pay_type
+
+  has_many :line_items, dependent: :destroy
+
+  PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
