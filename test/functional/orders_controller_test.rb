3f47287c89aa190ac3985b9534b5851427ad9be0
myappleguy <myappleguy@gmail.com>
2012-10-19 21:48:00 -0500

    all tests passing now after checkout added

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -18,11 +18,11 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
-    cart = Cart.create
-    session[:cart_id] = cart.id
-    li = LineItem.create
-    li.cart = cart
-    li.product(products(:ruby))
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
 
     get :new
     assert_response :success
@@ -33,7 +33,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
