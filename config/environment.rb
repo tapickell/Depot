bcab138538cd568ed45eedbfeaf4ed3853ad39d9
myappleguy <myappleguy@gmail.com>
2012-10-21 08:24:45 -0500

    added email config to enviroment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+# email config
+Depot::Application.configure do 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "depot.toddpickell.me",
+    authentication: "plain",
+    use_name: "myappleguy",
+    password: "Ic0de121926",
+    enable_starttls: true
+  }
+  
+end
