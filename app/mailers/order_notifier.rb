a6b65209b1044ab6842feea94ec6c90fd0fea395
myappleguy <myappleguy@gmail.com>
2012-10-21 09:54:31 -0500

    adding email functinoality

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,26 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "myappleguy@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+    @greeting = "Dear"
+
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @greeting = "Dear"
+
+    mail to: "to@example.org"
+  end
+end
