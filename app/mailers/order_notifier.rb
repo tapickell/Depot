23d58ae29cdd9f16a8ee9cec966a41edab011034
myappleguy <myappleguy@gmail.com>
2012-10-21 13:16:46 -0500

    adding more functionality with email

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -18,9 +18,10 @@ class OrderNotifier < ActionMailer::Base
   #
   #   en.order_notifier.shipped.subject
   #
-  def shipped
+  def shipped(order)
+    @order = order
     @greeting = "Dear"
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 end
