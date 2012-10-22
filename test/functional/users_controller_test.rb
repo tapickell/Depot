29649efab6dc2fde7e3e58a5a1bda0fee14995ba
myappleguy <myappleguy@gmail.com>
2012-10-21 20:26:59 -0500

    all tests pass

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   setup do
     @input_attributes = {
-      name: "sam"
-      password: "private"
+      name: "sam",
+      password: "private",
       password_confirmation: "private"
     }
 
